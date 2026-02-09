5ba17da96bd4222497f7d0a479266b30874e26b8
Bublitz <bublitz@hotmail.com>
2026-02-09 08:21:27 -0300

    add DotEnv config

--- mobile/src/app/shared/api/apiClient.ts
+++ mobile/src/app/shared/api/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_BASE_URL } from '@env';
 
 // Em Android físico, trocar localhost por IP da máquina.
 export const apiClient = axios.create({
-  baseURL: 'http://192.168.15.30:3000/api/v1',
+  baseURL: API_BASE_URL,
   timeout: 5000,
 });
